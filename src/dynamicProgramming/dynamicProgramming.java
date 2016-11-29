5d91d75e328c977ad787f7df5c62cb596ebeeff3
libaitaohong <492563822@qq.com>
2016-11-29 20:24:15 +0800

    LCS练习题

---
+++ src/dynamicProgramming/dynamicProgramming.java
@@ -0,0 +1,21 @@
+package dynamicProgramming;
+
+/**
+ * 工程: BAT2 包名: dynamicProgramming 类名: dynamicProgramming
+ * 作者: zhanghe
+ * 时间: 2016/11/29 20:22
+ * 题目:01背包
+ * 内容:一个背包有一定的承重cap，有N件物品，每件都有自己的价值，记录在数组v中，也都有自己的重量，
+ * 记录在数组w中，每件物品只能选择要装入背包还是不装入背包，要求在不超过背包承重的前提下，
+ * 选出物品的总价值最大。给定物品的重量w价值v及物品数n和承重cap。请返回最大总价值。
+ * 测试样例：[1,2,3],[1,2,3],3,6  返回：6
+ * 版本:
+ * 运行时间:
+ * 备注:
+ */
+public class dynamicProgramming {
+    public static void main(String[] args) {
+        dynamicProgramming t = new dynamicProgramming();
+        System.out.println();
+    }
+}
