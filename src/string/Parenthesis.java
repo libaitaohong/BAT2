a9795ae6ac4dafd03e37b43f8862f20bcef3debd
libaitaohong <492563822@qq.com>
2016-12-01 19:18:07 +0800

    Update Parenthesis.java

--- src/string/Parenthesis.java
+++ src/string/Parenthesis.java
@@ -5,6 +5,7 @@ package string;
  * 对于一个字符串，请设计一个算法，判断其是否为一个合法的括号串。
  * 给定一个字符串A和它的长度n，请返回一个bool值代表它是否为一个合法的括号串。
  * 27 ms,583K
+ 只有括号，有别的字符就返回FALSE
  */
 public class Parenthesis {
     public boolean chkParenthesis(String A, int n) {
